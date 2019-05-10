f99cc16b82cb50bb3797caf4b0f3df94556826ba
Stadin <50448509+Stadin@users.noreply.github.com>
2019-05-10 13:38:35 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Lets be DJ's!");
-
+		System.out.println("Made by Andreas");
 	}
 
 }
