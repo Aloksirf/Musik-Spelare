39375153297197154bdd59b2b9015b0244a7e4d2
HyabusaKiller <50486752+HyabusaKiller@users.noreply.github.com>
2019-05-10 16:56:44 +0200

    Add files via upload
    Hashing contains a public arraylist<Music> who will have an extended amount of functions

---
+++ Hashing.java
@@ -0,0 +1,25 @@
+package muplay;
+
+import java.util.ArrayList;
+
+/**
+ * är en konstruktor som lagrar musikfiler i en hashlista.
+ * @author Master booom
+ *
+ * @param <Music>
+ */
+public class Hashing<Music> {
+
+	public ArrayList<Music> HashList;
+	
+	public Hashing() {
+		HashList=null;
+	}
+	
+	public void hashAdd(Music song) {
+		
+		
+	}
+	
+	
+}
