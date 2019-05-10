418a83edc7a0133f75bfc1cddd96705f74943d00
Oliv3r123 <50491080+Oliv3r123@users.noreply.github.com>
2019-05-10 15:56:38 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 		System.out.println("Hello World!");
 		System.out.println("Lets be DJ's!");
 		System.out.println("Made by Andreas");
+		System.out.println("Hello, My name is Oliver!");
 	}
 
 }
