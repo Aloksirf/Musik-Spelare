4be7a823a3d1b7ff7810b2da1dc20f06a6834bf7
HyabusaKiller <n.kristrom@gmail.com>
2019-05-13 17:42:07 +0200

    Update PlayList.java

--- PlayList.java
+++ PlayList.java
@@ -7,7 +7,15 @@ public class PlayList {
 		System.out.println("asda");
 		
 	}
-	public Music[] loadPlayList(String name) {
+	public Music[] loadPlayList(String name) throws FileNotFoundException {
+		Scanner reader= new Scanner(new FileReader(new File(name+".txt")));
+		
+		while(reader.hasNext()) {
+			Music mus=();
+			
+			
+		}
+		
 		return null;
 	}
 	public void addToPlayList() {
