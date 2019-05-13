fa2fc4df9f0d3a76e8799de9745cfda2ff6a8086
HyabusaKiller <n.kristrom@gmail.com>
2019-05-13 19:39:33 +0200

    Update PlayList.java

--- PlayList.java
+++ PlayList.java
@@ -1,3 +1,4 @@
+package muplay;
 import java.io.*;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -10,26 +11,28 @@ import java.util.Scanner;
 
 public class PlayList {
 
-	ArrayList<Music> Songs=null;
-	String playList="";
+	static ArrayList<Music> Songs=new ArrayList<Music>();
+	static String playList="";
 	
-	public void savePlayList( String name) {
-		playList=name;
-		String output = "";
-		ArrayList<Music> CopySongs = Songs;
-		while(!Songs.isEmpty()) {
-			output = CopySongs.get(0).band+ ", " +CopySongs.get(0).song+ ", " + CopySongs.get(0).playtime + ", "+ CopySongs.get(0).fileCode +"/n";
-			CopySongs.remove(0);
-		}
-		
+	public static void savePlayList( String name) {
 		 PrintWriter filout=null;
 	      
 	      try{
-	      
-	         filout=new PrintWriter(new BufferedWriter(new FileWriter(name, false)));
+
+		filout=new PrintWriter(new BufferedWriter(new FileWriter(name+".txt", false)));
+		String output = "";
+		@SuppressWarnings("unchecked")
+		ArrayList<Music> CopySongs = (ArrayList<Music>) Songs.clone();
+		while(!CopySongs.isEmpty()) {
+			output = CopySongs.get(0).band+ ", " +CopySongs.get(0).song+ ", " + CopySongs.get(0).playtime + ", "+ CopySongs.get(0).fileCode;
+			CopySongs.remove(0);
+	         filout.println(output);
+		}
+		
+
 	         
 	      	
-	         filout.write(output);
+
 	      }
 	      	
 	      catch ( IOException e){}
@@ -42,7 +45,7 @@ public class PlayList {
 		
 		
 	}
-	public ArrayList<Music> loadPlayList(String name) throws FileNotFoundException {
+	public static ArrayList<Music> loadPlayList(String name) throws FileNotFoundException {
 		@SuppressWarnings("resource")
 		Scanner reader= new Scanner(new FileReader(new File(name+".txt")));
 		
@@ -69,10 +72,23 @@ public class PlayList {
 		return null;
 		
 	}
-	
-	public void printPlaylist() {
+	public static void printPlayList() {
 		for(int i =0; i<Songs.size();i++)
 		Songs.get(i).printInfo();
 	}
+	/*public static void main(String[] args) {
+		
+		try {
+			ArrayList<Music> test=loadPlayList("list");
+			savePlayList("test");
+			printPlayList();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}*/
 	
 }
