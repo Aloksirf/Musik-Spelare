bc303dd4344cf619b056047478adf7dbe2af3a4f
Aloksirf <50465514+Aloksirf@users.noreply.github.com>
2019-05-13 17:55:18 +0200

    Update PlayList.java

--- PlayList.java
+++ PlayList.java
@@ -1,10 +1,34 @@
+import java.util.ArrayList;
 
 public class PlayList {
 
 	
 	
-	public void savePlayList() {
-		System.out.println("asda");
+	public void savePlayList(ArrayList<E> Songs, String name) {
+		
+		String output = "";
+		while(!Songs.isEmpty()) {
+			output = Songs.remove(0) + /n;
+		}
+		
+		 PrintWriter filout=null;
+	      
+	      try{
+	      
+	         filout=new PrintWriter(new BufferedWriter(new FileWriter(name, true)));
+	         
+	      	
+	         filout.write(output);
+	      }
+	      	
+	      catch ( IOException e){}
+	      
+	      finally{
+	         filout.close();
+	      
+	      
+	      }
+		
 		
 	}
 	public Music[] loadPlayList(String name) throws FileNotFoundException {
