668e84bae9783abee976088ea764a1adc71fa5e2
HyabusaKiller <50486752+HyabusaKiller@users.noreply.github.com>
2019-05-13 17:18:28 +0200

    Add files via upload

---
+++ PlayList.java
@@ -0,0 +1,21 @@
+package muplay;
+
+public class PlayList {
+
+	
+	
+	public void savePlayList() {
+		
+	}
+	public Music[] loadPlayList(String name) {
+		return null;
+	}
+	public void addToPlayList() {
+		
+	}
+	public Music[] sortPlayList() {
+		return null;
+		
+	}
+	
+}
