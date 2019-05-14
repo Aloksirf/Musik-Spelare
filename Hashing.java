ffbca0777e9acf55d2792a4e4bfdee3ee81e119e
Stadin <50448509+Stadin@users.noreply.github.com>
2019-05-14 20:37:49 +0200

    Update Hashing.java

--- Hashing.java
+++ Hashing.java
@@ -1,30 +1,34 @@
-﻿
+
 import java.util.ArrayList;
 import java.util.Comparator;
 
 /**
  * är en konstruktor som lagrar musikfiler i en hashlista.
+ * 
  * @author Master booom
  *
  * @param <Music>
  */
 public class Hashing {
 
-	public Music[] hashSong;
-	public int currentSize;
-	
+	public static Music[] hashSong;
+	public static Music[] hashBand;
+	public static Music[] hashTime;
+	public static int currentSize;
+
 	public Hashing() {
 		hashSong = new Music[100];
 		currentSize = 0;
 	}
-	
+
 	public void add(Music song) {
-		doubleHashTabell();
+		doubleHashtabell();
 		int index = getIndex(song);
 		hashSong[index] = song;
 		currentSize++;
-		
+
 	}
+
 	/**
 	 * Method that finds the object in the hashtabell.
 	 */
@@ -49,64 +53,67 @@ public class Hashing {
 
 		return hashSong[temp];
 	}
-	
+
 	/**
-	 * Returns a music array with the songs from a chosen band. 
+	 * Returns a music array with the songs from a chosen band.
+	 * 
 	 * @param Name
 	 * @return
 	 */
-	public Music[] GetBand(String Name) {
-		int index= 0;
-		Music[] Array=new Music[150];
-		String Code=Name;
-		for(int i=0;i<Code.length() ; i++) {
-			index+=(int)Code.charAt(i);
+	public Music[] getBand(String Name) {
+		int index = 0;
+		Music[] Array = new Music[150];
+		String Code = Name;
+		for (int i = 0; i < Code.length(); i++) {
+			index += (int) Code.charAt(i);
 		}
-		while(HashBand[index].band.compareTo(Name)!=0) {
+		while (hashBand[index].band.compareTo(Name) != 0) {
 			index++;
 		}
-		for(int i=0;HashBand[index+i].band.compareTo(Name)==0;i++) {
-			Array[i]=HashBand[index+i];
+		for (int i = 0; hashBand[index + i].band.compareTo(Name) == 0; i++) {
+			Array[i] = hashBand[index + i];
 		}
 		return Array;
 	}
+
 	/**
 	 * Returns a Music with the same song name.
+	 * 
 	 * @param Name
 	 * @return
 	 */
-	public Music GetSong(String Name) {
-		int index= 0;
-		String Code=Name;
-		for(int i=0;i<Code.length() ; i++) {
-			index+=(int)Code.charAt(i);
+	public Music getSong(String Name) {
+		int index = 0;
+		String Code = Name;
+		for (int i = 0; i < Code.length(); i++) {
+			index += (int) Code.charAt(i);
 		}
-		while(HashSong[index].song.compareTo(Name)!=0) {
+		while (hashSong[index].song.compareTo(Name) != 0) {
 			index++;
 		}
-		return HashBand[index];
+		return hashBand[index];
 	}
-	
-	
+
 	/**
 	 * Returns a Music array with songs with the same PlayTime.
+	 * 
 	 * @param time
 	 * @return
 	 */
-	public Music[] GetTime(long time) {
-		  int index= (int)time;
-		Music[] Array=new Music[40];
+	public Music[] getTime(long time) {
+		int index = (int) time;
+		Music[] Array = new Music[40];
 
-		while(HashTime[index].playtime-time!=0) {
+		while (hashTime[index].playtime - time != 0) {
 			index++;
 		}
-		for(int i=0;HashTime[index+i].playtime-time==0; i++) {
-			Array[i]=HashTime[index+i];
+		for (int i = 0; hashTime[index + i].playtime - time == 0; i++) {
+			Array[i] = hashTime[index + i];
 		}
-		
+
 		return Array;
 	}
-	
+
 	public static int getIndex(Music obj) {
 		String a = obj.song;
 
@@ -130,19 +137,19 @@ public class Hashing {
 
 		return temp;
 	}
-	
+
 	public static void doubleHashtabell() {
-		if(overLoad()) {
-			String[] temp = new String[hashSong.length*2];
-			for(int i = 0; i < hashSong.length; i++) {
+		if (overLoad()) {
+			Music[] temp = new Music[hashSong.length * 2];
+			for (int i = 0; i < hashSong.length; i++) {
 				temp[i] = hashSong[i];
 			}
 			hashSong = temp;
-		}		
+		}
 	}
-	
+
 	public static boolean overLoad() {
-		return (currentSize+1.0)/(hashSong.length+0.0) > 0.5;
+		return (currentSize + 1.0) / (hashSong.length + 0.0) > 0.5;
 	}
-	
+
 }
