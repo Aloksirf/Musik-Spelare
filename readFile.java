386c4db776f7e2517db4ccf78c84e13862e71823
Stadin <50448509+Stadin@users.noreply.github.com>
2019-05-16 01:12:44 +0200

    Update readFile.java

--- readFile.java
+++ readFile.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class readFile {
 	Scanner in = new Scanner(System.in);
 	PrintWriter write = null;
+	
 
 	/**
 	 * Reads in a file and creates new music objects to an ArrayList
@@ -15,13 +16,11 @@ public class readFile {
 	public static void readInFile(Hashing toHash) {
 		try {
 			Scanner read = new Scanner(new File("Library.txt"));
-			System.out.println("Current Music Library:");
 			while (read.hasNext()) {
 				String line = read.nextLine();
 				String[] split = line.split(", ");
 				Music mus = new Music(split[0], split[1], Integer.parseInt(split[2]), split[3]);
 				toHash.add(mus);
-				System.out.println(mus.toString());
 			}
 
 			read.close();
