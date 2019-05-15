abcf7c3b3a34015093e9ee0c9e5fc91b7ac12221
Stadin <50448509+Stadin@users.noreply.github.com>
2019-05-16 01:13:09 +0200

    Update MainFile.java

--- MainFile.java
+++ MainFile.java
@@ -6,6 +6,9 @@ public class MainFile {
 	public static void main(String[] args) {
 		library = new Hashing();
 		readFile.readInFile(library);
+		MusicPlayer.getPlayList = "Library.txt";
+		System.out.println("------------- Printing Library File -------------");
+		MusicPlayer.myPlayList.loadPlayList("Library");
 		MusicPlayer m = new MusicPlayer();
 		m.setVisible(true);
 	}
