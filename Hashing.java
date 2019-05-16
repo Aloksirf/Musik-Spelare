27c8dae6e83ad0c92884580f5ecf3582e3deb5aa
Oliv3r123 <50491080+Oliv3r123@users.noreply.github.com>
2019-05-16 14:44:13 +0200

    Update Hashing.java

--- Hashing.java
+++ Hashing.java
@@ -1,29 +1,41 @@
 /**
- * är en konstruktor som lagrar musikfiler i en hashlista.
+ * Class that creates a hashtabell that stores music objects. 
  * 
- * @author Master booom
- *
+ * @author Andreas Stadin, Nicklas Kriström, Vidar Hårding and Oliver Olsson
  * @param <Music>
  */
 public class Hashing {
 
 	public static Music[] hashSong;
 	public static int currentSize;
-
+	
+	/**
+	* Constructor that creates an array with 100 empty spaces and a
+	* counter that will keep track of amount of objects that has been added.
+	*/
 	public Hashing() {
 		hashSong = new Music[100];
 		currentSize = 0;
 	}
-
+	
+	/**
+	* Method that adds a music object to the hashtabell.
+	*
+	* @param obj The music object that will be added.
+	*/
 	public void add(Music obj) {
 		doubleHashtabell();
-		int index = getIndex(obj.song);
+		int index = calculateIndex(obj.song);
 		hashSong[index] = obj;
 		currentSize++;
 	}
 
 	/**
-	 * Method that finds the object in the hashtabell.
+	 * Method that finds a specified object in the hashtabell.
+	 * 
+	 * @param a The name of the song that will be retrived.
+	 * @return The music object if it exist and null if object
+	 * isn't found.
 	 */
 	public Music getMusic(String a) {
 		int index = 0;
@@ -47,7 +59,7 @@ public class Hashing {
 					temp = temp % hashSong.length;
 				}
 			} else if (i++ == 100) {
-				System.out.println("Song not found.");
+				System.out.println("\nSong not found.");
 				return null;
 			}
 		}
@@ -55,7 +67,13 @@ public class Hashing {
 
 	}
 
-	public static int getIndex(String a) {
+	/**
+	* Method that calculates the index at wich the object will be added.
+	*
+	* @param a Name of the song that will be added.
+	* @return temp The index at wich the object will be added.
+	*/
+	private static int calculateIndex(String a) {
 
 		int index = 0;
 		for (int i = 0; i < a.length(); i++) {
@@ -63,21 +81,21 @@ public class Hashing {
 			index += letterValue * (int) Math.pow(2, (a.length() - i));
 		}
 
-		int indexValue = index % hashSong.length;
-
+		int temp = index % hashSong.length;
 		int n = 1;
-
-		int temp = indexValue;
-
-		while (hashSong[temp] != null) {
-			temp = indexValue + (int) Math.pow(n, 2);
+		int indexValue = temp;
+		while (hashSong[indexValue] != null) {
+			indexValue = temp + (int) Math.pow(n, 2);
 			n++;
-			temp = temp % hashSong.length;
+			indexValue = indexValue % hashSong.length;
 		}
-		return temp;
+		return indexValue;
 	}
-
-	public static void doubleHashtabell() {
+	
+	/**
+	* Method that doubles the array if the array if the array becomes 50% full. 
+	*/
+	private static void doubleHashtabell() {
 		if (overLoad()) {
 			Music[] temp = new Music[hashSong.length * 2];
 			for (int i = 0; i < hashSong.length; i++) {
@@ -86,8 +104,11 @@ public class Hashing {
 			hashSong = temp;
 		}
 	}
-
-	public static boolean overLoad() {
+	
+	/**
+	*Method that checks if the array will be 50% full after next object has been added. 
+	*/
+	private static boolean overLoad() {
 		return (currentSize + 1.0) / (hashSong.length + 0.0) > 0.5;
 	}
 
