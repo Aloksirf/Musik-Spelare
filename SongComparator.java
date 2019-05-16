26bb5020621620018ec799d4dd67c22e848718d6
Oliv3r123 <50491080+Oliv3r123@users.noreply.github.com>
2019-05-16 14:58:16 +0200

    Update SongComparator.java

--- SongComparator.java
+++ SongComparator.java
@@ -1,7 +1,18 @@
 import java.util.Comparator;
-
+/**
+* Class that that implements Comparator and compares two music objects by the name of the songs.
+*
+* @author Andreas Stadin, Nickla Kristörm, Vidar Hårding and Oliver Olsson.
+*/
 public class SongComparator implements Comparator<Music> {
 
+	/**
+	* Method that compares two music objects by name of the songs.
+	*
+	* @param m1 first music object.
+	* @param m2 second music object.
+	* @return integer with compareTo-method.
+	*/
 	public int compare(Music o1, Music o2) {
 		return o1.song.compareTo(o2.song);
 	}
