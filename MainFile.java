3d2eab677b8952cf612d5ef1ececa5b7d53a453f
Stadin <50448509+Stadin@users.noreply.github.com>
2019-05-16 14:19:10 +0200

    Update MainFile.java

--- MainFile.java
+++ MainFile.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.util.Scanner;
 
 public class MainFile {
 
@@ -5,11 +7,32 @@ public class MainFile {
 	
 	public static void main(String[] args) {
 		library = new Hashing();
-		readFile.readInFile(library);
-		MusicPlayer.getPlayList = "Library.txt";
+		readInFile(library);
+		MusicPlayer m = new MusicPlayer();
 		System.out.println("------------- Printing Library File -------------");
 		MusicPlayer.myPlayList.loadPlayList("Library");
-		MusicPlayer m = new MusicPlayer();
 		m.setVisible(true);
+	}
+	
+	/**
+	 * Reads in a file and creates new music objects to an ArrayList
+	 * 
+	 * @param fileName The name of the file.
+	 * @return The ArrayList filled with music-objects.
+	 */
+	public static void readInFile(Hashing toHash) {
+		
+		try {
+			Scanner read = new Scanner(new File("Library.txt"));
+			while (read.hasNext()) {
+				String line = read.nextLine();
+				String[] split = line.split(", ");
+				Music mus = new Music(split[0], split[1], Integer.parseInt(split[2]), split[3]);
+				toHash.add(mus);
+			}
+			read.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
 }
