5405e73fd239e7f55c507067b3f47067e7e8fcf0
Oliv3r123 <50491080+Oliv3r123@users.noreply.github.com>
2019-05-16 14:51:03 +0200

    Update BandComparator.java

--- BandComparator.java
+++ BandComparator.java
@@ -1,7 +1,18 @@
 import java.util.Comparator;
-
+/**
+* Class that that implements Comparator and compares two music objects by the bandname.
+*
+* @author Andreas Stadin, Nickla Kristörm, Vidar Hårding and Oliver Olsson.
+*/
 public class BandComparator implements Comparator<Music> {
-
+	
+	/**
+	* Method that compares two music objects by band.
+	*
+	* @param m1 first music object.
+	* @param m2 second music object.
+	* @return integer with compareTo-method.
+	*/
 	public int compare(Music m1, Music m2) {
 		return m1.band.compareTo(m2.band);
 	}
